659ec2b2940c1d0844db2d732d77dc234cfca79f
Joel Garcia <jgarcia95@hotmail.com>
2017-05-28 20:30:36 -0400

    added custom classes

---
+++ social/Constans.swift
@@ -0,0 +1,11 @@
+//
+//  Constans.swift
+//  social
+//
+//  Created by Joel Garcia on 5/28/17.
+//  Copyright © 2017 Joel Garcia. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 128 / 255
---
+++ social/FancyField.swift
@@ -0,0 +1,32 @@
+//
+//  FancyField.swift
+//  social
+//
+//  Created by Joel Garcia on 5/28/17.
+//  Copyright © 2017 Joel Garcia. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+    layer.borderWidth = 1.0
+    layer.cornerRadius = 5.0
+    
+    
+  }
+
+  override func textRect(forBounds bounds: CGRect) -> CGRect {
+    return bounds.insetBy(dx: 10, dy: 5)
+    
+  }
+  
+  override func editingRect(forBounds bounds: CGRect) -> CGRect {
+       return bounds.insetBy(dx: 10, dy: 5)
+  }
+  
+}
